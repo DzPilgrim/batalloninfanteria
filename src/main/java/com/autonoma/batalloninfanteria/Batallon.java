6ab1b7a9642ad132b7fec573f719c449510f2207
Oswaldo <oswaldodaza14@gmail.com>
2025-11-25 08:09:18 -0500

    Implementados getters y setters de la clase Batallon

--- src/main/java/com/autonoma/batalloninfanteria/Batallon.java
+++ src/main/java/com/autonoma/batalloninfanteria/Batallon.java
@@ -22,4 +22,46 @@ public class Batallon {
     private TipoBatallon tipo;
     private ArrayList<Peloton> pelotones;
     private static String codigoOTAN ="";
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public TipoBatallon getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoBatallon tipo) {
+        this.tipo = tipo;
+    }
+
+    public ArrayList<Peloton> getPelotones() {
+        return pelotones;
+    }
+
+    public void setPelotones(ArrayList<Peloton> pelotones) {
+        this.pelotones = pelotones;
+    }
+
+    public static String getCodigoOTAN() {
+        return codigoOTAN;
+    }
+
+    public static void cambiarCodigoOTAN(String nuevoCodigo) {
+        Batallon.codigoOTAN = nuevoCodigo;
+    }
+    
+    
 }
